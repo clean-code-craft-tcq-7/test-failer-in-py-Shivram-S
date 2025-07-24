bdd1b9cdb0e082abee12340ab713ba2c08ff147f
Shivram-S <44567173+Shivram-S@users.noreply.github.com>
2025-07-24 20:06:41 +0530

    Update weatherreport.py - New stubs

--- weatherreport.py
+++ weatherreport.py
@@ -1,13 +1,31 @@
-
-
-def sensorStub():
+def sensorStubRain():
     return {
         'temperatureInC': 50,
         'precipitation': 70,
         'humidity': 26,
         'windSpeedKMPH': 52
     }
-
+def sensorStubPrep():
+    return {
+        'temperatureInC': 50,
+        'precipitation': 70,
+        'humidity': 26,
+        'windSpeedKMPH': 49
+    }
+def sensorStubCold():
+    return {
+        'temperatureInC': 20,
+        'precipitation': 70,
+        'humidity': 26,
+        'windSpeedKMPH': 49
+    }
+def sensorStubSunny():
+    return {
+        'temperatureInC': 27,
+        'precipitation': 10,
+        'humidity': 26,
+        'windSpeedKMPH': 49
+    }
 
 def report(sensorReader):
     readings = sensorReader()
@@ -16,29 +34,32 @@ def report(sensorReader):
     if (readings['temperatureInC'] > 25):
         if readings['precipitation'] >= 20 and readings['precipitation'] < 60:
             weather = "Partly Cloudy"
-        elif readings['windSpeedKMPH'] > 50:
+        elif readings['windSpeedKMPH'] > 50 or readings['precipitation'] >= 60:
             weather = "Alert, Stormy with heavy rain"
+    else : 
+        weather = "Cold day"     
     return weather
 
 
-def testRainy():
-    weather = report(sensorStub)
-    print(weather)
-    assert("rain" in weather)
-
-
-def testHighPrecipitation():
-    # This instance of stub needs to be different-
-    # to give high precipitation (>60) and low wind-speed (<50)
-
-    weather = report(sensorStub)
-
-    # strengthen the assert to expose the bug
-    # (function returns Sunny day, it should predict rain)
-    assert(len(weather) > 0);
-
+def testWeather(StubWeather):
+    if StubWeather == 'rain':
+        weather = report(sensorStubRain)
+        assert("rain" in weather)
+    elif StubWeather == 'prep':
+        weather = report(sensorStubPrep)
+        assert("rain" in weather)
+    elif StubWeather == 'cold':
+        weather = report(sensorStubCold)
+        assert("Cold" in weather)
+    elif StubWeather == 'sun':
+        weather = report(sensorStubSunny)
+        assert("Sunny" in weather)
+    else:
+        assert(True == False)
 
 if __name__ == '__main__':
-    testRainy()
-    testHighPrecipitation()
-    print("All is well (maybe!)");
+    testWeather('rain')
+    testWeather('prep')
+    testWeather('sun')
+    testWeather('cold')
+    print("All is well")
