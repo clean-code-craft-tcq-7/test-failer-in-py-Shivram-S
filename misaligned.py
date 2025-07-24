17eeed769dbd59360e48ce05b4e4f51917310d82
Shivram-S <44567173+Shivram-S@users.noreply.github.com>
2025-07-24 19:19:20 +0530

    Update misaligned.py - Refactoring + fix

--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,27 @@
+major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+
+def getIndex(i,j):
+    return i * 5 + j + 1
+
+def getMajorMinor(i,j):
+    return major_colors[i], minor_colors[j]
 
 def print_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
+            maj, mino = getMajorMinor(i,j)
+            print(f'{getIndex(i,j)} | {maj} | {mino}')
     return len(major_colors) * len(minor_colors)
 
 
 result = print_color_map()
 assert(result == 25)
-print("All is well (maybe!)")
+assert(getIndex(0,0) == 1)
+assert(getIndex(0,1) == 2)
+assert(getIndex(4,4) == 25)
+assert(getIndex(2,2) == 13)
+assert(getMajorMinor(0,0) == ('White','Blue'))
+assert(getMajorMinor(0,3) == ('White','Brown'))
+assert(getMajorMinor(3,4) == ('Yellow','Slate'))
+print("All is well")
