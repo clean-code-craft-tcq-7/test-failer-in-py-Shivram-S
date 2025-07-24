74dacec3f7c8e9fbd802ed2151248ec18af1d223
Shivram-S <44567173+Shivram-S@users.noreply.github.com>
2025-07-24 19:28:30 +0530

    Changed Stub + Fixing the bug in alert

--- alerter.py
+++ alerter.py
@@ -2,10 +2,14 @@ alert_failure_count = 0
 
 def network_alert_stub(celcius):
     print(f'ALERT: Temperature is {celcius} celcius')
+    retVal = 0
     # Return 200 for ok
     # Return 500 for not-ok
-    # stub always succeeds and returns 200
-    return 200
+    if celcius > 200:
+        retVal = 500
+    else:
+        retVal = 200
+    return retVal
 
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
@@ -16,10 +20,12 @@ def alert_in_celcius(farenheit):
         # However, this code doesn't count failures!
         # Add a test below to catch this bug. Alter the stub above, if needed.
         global alert_failure_count
-        alert_failure_count += 0
+        alert_failure_count += 1
 
 
 alert_in_celcius(400.5)
+assert(alert_failure_count == 1)
 alert_in_celcius(303.6)
+assert(alert_failure_count == 1)
 print(f'{alert_failure_count} alerts failed.')
-print('All is well (maybe!)')
+print('All is well')
